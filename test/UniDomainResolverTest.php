ae1c6fa8c2a199932dbd507b3c3813c38dac6fe7
Paul Water <p.r.water@setbased.nl>
2017-05-10 12:36:52 +0200

    Renamed test class.

--- test/UniDomainResolverTest.php
+++ test/UniDomainResolverTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 use SetBased\Abc\DomainResolver\UniDomainResolver;
 
 //----------------------------------------------------------------------------------------------------------------------
-class DomainResolverTest extends TestCase
+class UniDomainResolverTest extends TestCase
 {
   //--------------------------------------------------------------------------------------------------------------------
   /**
